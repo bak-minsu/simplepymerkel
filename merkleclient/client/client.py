54395608df584169898ca394c5110981fac7abec
Min Su Park <minsupark@live.com>
2020-05-08 22:23:58 -0400

    Now you can send arbitrary files

--- merkleclient/client/client.py
+++ merkleclient/client/client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import os
 
 class Client:
     def __init__(self):
@@ -11,23 +12,77 @@ class Client:
     def setup(self):
         self.debug = True
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if(self.debug): print("Setup complete.")
-    
-    def send_message(self, message):
-        self.socket.sendall(message)
-    
-    def receive_message(self):
-        return self.socket.recv(1024)
-
-    def run(self):
         self.port = 55555
         # self.address = "192.168.0.100"
         self.address = "127.0.0.1"
+        if(self.debug): print("Setup complete.")
+    
+    def send_message(self, message):
+        print("[Client]: {0}".format(message))
+        self.socket.sendall(str.encode(message))
+    
+    def receive_message(self):
+        message = self.socket.recv(1024).decode("utf-8")
+        print("[Server]: {0}".format(message))
+    
+    def upload_file(self, filepath):
+        basename = os.path.basename(filepath)
+        print("Uploading File: {}".format(basename))
+        with open(filepath, 'rb') as file2send:
+            size = self.socket.sendfile(file2send)
+            print(size)
+            # while data := file2send.read(1024):
+            #     self.socket.sendall(data)
+            # self.socket.sendall(b"Badly Implemented End of File Message")
+        print("Uploaded File: {}".format(basename))
+
+    def get_filepath(self):
+        done = False
+        path = None
+        while not done:
+            path = input("Input an empty message to exit.\n[Enter File Path]: ").strip()
+            if os.path.exists(path):
+                basename = os.path.basename(path)
+                size = os.path.getsize(path)
+                print("Sending File '{0}' of size {1}".format(basename, size))
+                self.send_message("{0}:{1}".format(basename, size))
+                done = True
+            elif path == "":
+                path = None
+                self.send_message("No More Files")
+                done = True
+            else:
+                print("Path does not exist!")
+        return path
+
+    def send_files(self):
+        done = False
+        while not done:
+            path = self.get_filepath()
+            if path is not None:
+                self.upload_file(path)
+            else:
+                done = True
+
+    def run(self):
         self.socket.connect((self.address, self.port))
-        if(self.debug): print("Connecting to: {0}".format(self.socket.getsockname())) 
-        self.send_message(b"Hello World!")
-        data = self.receive_message()
-        print('Received: {0}'.format(data))
+        if self.debug: print("Connecting to: {0}".format(self.socket.getsockname()))
+        self.receive_message()
+        self.cli()
+
+    def cli(self):
+        done = False
+        while not done:
+            command = input("Input Command> ").strip()
+            list_of_commands = ["sendfiles", "exit"]
+            if command in list_of_commands:
+                self.send_message(command)
+                if command == "sendfiles":
+                    self.send_files()
+                elif command == "exit":
+                    done = True
+            else:
+                print("Given command does not exist.")
     
     def stop(self):
         self.socket.close()
--- merkleserver/server/server.py
+++ merkleserver/server/server.py
@@ -9,46 +9,94 @@ class Server:
 
     @classmethod
     def setup(cls):
-        Server.debug = True
-        Server.tree = MerkleTree()
-        Server.file_dir = os.path.join(os.path.expanduser("~"), "MerkleFileStorage")
-        if not os.path.exists(Server.file_dir): os.mkdir(Server.file_dir)
-        Server.socket = socket.socket()
-        Server.port = 55555
-        if(Server.debug): print("Setup Complete") 
-
-    @classmethod
-    def start_server(cls):
-        Server.setup()
-        Server.run()
-        Server.stop()
-
-    @classmethod
-    def run(cls):
-        Server.socket.bind(("", Server.port))
-        if(Server.debug): print("Binded to: {0}".format(Server.socket.getsockname())) 
-        Server.socket.listen()
-        if(Server.debug): print("Listening for Connections...") 
-        conn, addr = Server.socket.accept()
-        with conn:
-            print("Accepted Connection from {0}".format(addr))
-            while True:
-                init_msg = conn.recv(1024)
-                if not init_msg:
-                    break
-                else:
-                    print("Message from client: {0}".format(init_msg))
-                    print("Sending Message: Hello!")
-                    conn.sendall(b"Hello!")
+        cls.debug = True
+        cls.tree = MerkleTree()
+        cls.file_dir = os.path.join(os.path.expanduser("~"), "MerkleFileStorage")
+        if not os.path.exists(cls.file_dir): os.mkdir(cls.file_dir)
+        cls.socket = socket.socket()
+        cls.port = 55555
+        if cls.debug: print("Setup Complete") 
 
     @classmethod
     def stop(cls):
-        Server.socket.close()
+        cls.socket.close()
+
+    @classmethod
+    def start_server(cls):
+        cls.setup()
+        cls.run()
+        cls.stop()
+
+    @classmethod
+    def run(cls):
+        connection = cls.wait_for_connection()
+        cls.handle_connection(connection)
+
+    @classmethod
+    def wait_for_connection(cls):
+        cls.socket.bind(("", cls.port))
+        if cls.debug: print("Binded to: {0}".format(cls.socket.getsockname())) 
+        cls.socket.listen()
+        if cls.debug: print("Listening for Connections...")
+        return cls.socket.accept()
+
+    @classmethod
+    def send_message(cls, message, conn_object):
+        print("[Server]: {0}".format(message))
+        conn_object.sendall(str.encode(message))
+    
+    @classmethod
+    def receive_message(cls, conn_object):
+        message = conn_object.recv(1024).decode("utf-8")
+        print("[Client]: {0}".format(message))
+        return message
+
+    @classmethod
+    def download_file(cls, filename, size, conn_object):
+        path = os.path.join(cls.file_dir, filename)
+        print("Downloading File: {0}".format(filename))
+        with open(path, 'wb') as received_file:
+            data = conn_object.recv(size)
+            received_file.write(data)
+        print("Downloaded File: {0}".format(filename))
+        
+
+    @classmethod
+    def receive_files(cls, conn_object):
+        print("Set to receive files.")
+        receive_more = True
+        while receive_more:
+            message = cls.receive_message(conn_object)
+            if message != "No More Files":
+                filename, size = message.split(":")
+                print("Received File '{0}' of size {1}".format(filename, size))
+                cls.download_file(filename, int(size), conn_object)
+            else: receive_more = False
+        print("Completed receiving Files.")
+
+    @classmethod
+    def handle_connection(cls, connection):
+        conn_object, addr = connection
+        with conn_object:
+            print("Accepted Connection from {0}".format(addr))
+            cls.send_message("Hello Client! Send a Command!", conn_object)
+            cls.cli(conn_object)
+    
+    @classmethod
+    def cli(cls, conn_object):
+        done = False
+        while not done:
+            command = cls.receive_message(conn_object)
+            print("Recieved Command: {0}".format(command))
+            if command == "sendfiles":
+                cls.receive_files(conn_object)
+            elif command == "exit":
+                done = True
 
     @classmethod
     def print_tree(cls):
-        if(Server.tree is not None):
-            print(Server.tree)
+        if cls.tree is not None:
+            print(cls.tree)
 
 def __main__():
     Server.start_server()
